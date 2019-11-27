e6ec77dc61d525ff24ec08da7824cb4db922709a
HADMARINE <joonlee031@live.com>
2019-11-27 21:52:08 +0900

    Update OnlineStore.ts
    Create OnlineStore Schema

--- src/models/OnlineStore.ts
+++ src/models/OnlineStore.ts
@@ -1,16 +1,39 @@
 import { Schema, model, Document, models } from "mongoose";
 
 const OnlineStoreSchema = new Schema({
-    title
-    content
-
-    
-})
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  cost: {
+    type: String,
+    required: true
+  },
+  remain: {
+    type: Number,
+    required: true
+  },
+  img: {
+    type: String,
+    default: ""
+  }
+});
 
 export interface OnlineStoreDocument extends Document {
-
+  title: string;
+  content: string;
+  cost: string;
+  remain: number;
 }
 
-const OnlineStore = model<OnlineStoreDocument>("onlineStore",OnlineStoreSchema);
+const OnlineStore = model<OnlineStoreDocument>(
+  "onlineStore",
+  OnlineStoreSchema
+);
 
 export default OnlineStore;
