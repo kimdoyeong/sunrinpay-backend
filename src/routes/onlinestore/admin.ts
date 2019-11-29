f36d22c8004c7dd0c7bc74f23b89a1c6803b2dc2
HADMARINE <joonlee031@live.com>
2019-11-30 01:31:39 +0900

    Update admin.ts
    prettier

--- src/routes/onlinestore/admin.ts
+++ src/routes/onlinestore/admin.ts
@@ -4,26 +4,36 @@ import createError from "../../lib/error/createError";
 import User, { UserDocument } from "../../models/User";
 import { authorized, adminAuthorized } from "../../lib/middlewares/auth";
 import OnlineStore from "../../models/Product";
-import {uploadImage} from '../../lib/ImageUpload'
+import { uploadImage } from "../../lib/ImageUpload";
 import Product from "../../models/Product";
 
 const router = Router();
 
-router.post('/input',adminAuthorized,wrapAsync(async(req,res)=> {
-    const { title , content , cost , stock , img } = req.body;
+router.post(
+  "/input",
+  adminAuthorized,
+  wrapAsync(async (req, res) => {
+    const { title, content, cost, stock, img } = req.body;
 
     const product = new Product({
-        title,content,cost,stock,img : ""
-    })
+      title,
+      content,
+      cost,
+      stock,
+      img: ""
+    });
 
     await product.save();
-    
-    uploadImage(img, product._id);
 
-    await Product.findOneAndUpdate({_id: product._id}, {$set:{img: process.env.HTTP_URI || "localhost"}});
+    await uploadImage(img, product._id);
 
-    res.status(200).json({state:true})
+    await Product.findOneAndUpdate(
+      { _id: product._id },
+      { $set: { img: process.env.HTTP_URI || "localhost" } }
+    );
 
-}));
+    res.status(200).json({ state: true });
+  })
+);
 
 export default router;
