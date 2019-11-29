d228789b754586456a4cc6b9e3f941d1de5b3661
HADMARINE <joonlee031@live.com>
2019-11-30 08:53:20 +0900

    mongodb connect to server

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -1,21 +1,25 @@
 import { connect, set } from "mongoose";
 import env from "./env";
+require("dotenv").config();
 
-const uri = `mongodb://localhost/sunrinpay${
-  env === "production" ? "" : `_${env}`
-}`;
+const uri = `${process.env.MONGO_URI}${env === "production" ? "" : `_${env}`}`;
 
 if (env === "development") {
   set("debug", true);
 }
+
+const auth = {
+  user: process.env.MONGO_ID,
+  pass: process.env.MONGO_PASS
+};
+
 async function connectDB() {
-  await connect(
-    uri,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  );
+  await connect(uri, {
+    ...auth,
+    authSource: "admin",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 }
 
 export default connectDB;
