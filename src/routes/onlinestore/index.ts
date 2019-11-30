f2c2d85772529ae8b053820880ed8b8d0314d516
HADMARINE <joonlee031@live.com>
2019-11-30 13:45:22 +0900

    /onlinestore

--- src/routes/onlinestore/index.ts
+++ src/routes/onlinestore/index.ts
@@ -14,16 +14,31 @@ router.get("/", async (req, res, next) => {
     if (!onlineStore) {
       createError("데이터가 없습니다.", 404);
     }
-    onlineStore.map((data: any)=>{
-      if(data.stock !== 0){
+    onlineStore.map((data: any) => {
+      if (data.stock !== 0) {
         sendArray.push(data);
       }
-    })
+    });
     res.status(200).json({ data: sendArray });
   } catch (error) {
     next(error);
   }
 });
 
+router.get("/:product", async (req, res, next) => {
+  const product = req.params.product;
+
+  try {
+    const onlineStoreData: Object = await OnlineStore.findOne({
+      title: product
+    });
+    if (!onlineStoreData) {
+      return createError("데이터가 없습니다.", 404);
+    }
+    res.status(200).json({ data: onlineStoreData });
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
